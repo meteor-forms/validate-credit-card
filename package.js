859cf152a4fa2091ab89aa874e622a648c5e82be
Eugene Traytyak <evgen.traytyak@gmail.com>
2015-10-16 14:27:34 +0300

    Add credit card validation

--- package.js
+++ package.js
@@ -1,24 +1,20 @@
 Package.describe({
-  name: 'forms:validate-credit-card',
-  version: '0.0.1',
-  // Brief, one-line summary of the package.
-  summary: '',
-  // URL to the Git repository containing the source code for this package.
-  git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md'
+    name: 'forms:validate-credit-card',
+    version: '0.0.1',
+    summary: 'Validate Credit Card Fields in Meteor Forms',
+    git: 'https://github.com/meteor-forms/validate-credit-card',
+    documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.2.0.2');
-  api.use('ecmascript');
-  api.addFiles('validate-credit-card.js');
+    api.versionsFrom('1.2.0.2');
+    api.use(['skinnygeek1010:validate-form@1.0.0']);
+    api.export('ValidateForm', 'client');
+    api.addFiles('validate-credit-card.js', 'client');
 });
 
 Package.onTest(function(api) {
-  api.use('ecmascript');
-  api.use('tinytest');
-  api.use('forms:validate-credit-card');
-  api.addFiles('validate-credit-card-tests.js');
+    api.use('tinytest');
+    api.use('forms:validate-credit-card');
+    api.addFiles('validate-credit-card-tests.js');
 });
--- validate-credit-card.js
+++ validate-credit-card.js
@@ -1,1 +1,26 @@
-// Write your package code here!
+ValidateForm.addValidator('data-credit-card', function ($el, instance) {
+    var value = $el.val().split(' ').join('');
+
+    var Visa = /^4[0-9]{12}(?:[0-9]{3})?$/gm;
+    var MasterCard = /^(5[1-5][0-9]{14}|3[47][0-9]{13})$/gm;
+    var AmericanExpress = /^3[47][0-9]{13}?$/gm;
+    var DinersClub = /^3(?:0[0-5]|[68][0-9])[0-9]{11}?/gm;
+    var Discover = /^6(?:011|5[0-9]{2})[0-9]{12}?/gm;
+    var JCB = /^(?:2131|1800|35\d{3})\d{11}?/gm;
+
+    var creditCards = [Visa, MasterCard, AmericanExpress, DinersClub, Discover, JCB];
+
+    var isValid = creditCards.some(function(creditCard) {
+        return value.match(creditCard);
+    });
+
+    if (isValid) {
+        instance._showSuccess();
+        instance.log("[ValidateForm] credit card success", instance.el);
+    } else {
+        instance._showError("Credit card don't match.");
+        instance.log("[ValidateForm] credit card failed", instance.el);
+    }
+
+    instance._validations.push(isValid);
+});
